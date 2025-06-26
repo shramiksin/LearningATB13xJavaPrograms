0027dc99300e835cc5f973986abba9fb85af96b6
shramiksin <shramiksin1712@gmail.com>
2025-06-26 17:18:08 +0530

    New adn Lab014 added

---
+++ src/ex_02_Java_Basics/Lab014_Variables.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main() {
+}
